99bc8d80d31d12ff1201f3cb3374613aac5ab089
josh <21151212+joshuakan9@users.noreply.github.com>
2024-08-13 13:14:34 -0700

    java doc

--- src/main/java/org/example/controller/NozamaDatabase.java
+++ src/main/java/org/example/controller/NozamaDatabase.java
@@ -11,13 +11,12 @@ public class NozamaDatabase {
     public Connection connect() {
         String url = "jdbc:mysql://localhost:3306/tcss_445_ecommerce_project";
         String user = "root";
-        String password = "Rock;RevivalS2029Camille";
+        String password = "password";
         Connection con = null;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected");
 
         } catch (ClassNotFoundException e1) {
             e1.printStackTrace();
